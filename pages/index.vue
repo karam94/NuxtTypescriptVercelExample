84b96b955a038e7de32b4558354792b2777e266e
Karam Kabbara <contact@karam.io>
2021-02-01 11:04:32 +0000

    Using axios to call functions

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,18 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">NuxtTypescriptVercelExample</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="text-4xl">NuxtTypescriptVercelExample</h1>
+
+      <ul>
+        <li v-for="(content, index) in contents" :key="index">
+          {{ content }}
+        </li>
+      </ul>
+
+      <pre>
+        Pre-rendered time: {{ time }}
+        Server rendered?: {{ server }}
+      </pre>
     </div>
   </div>
 </template>
@@ -28,7 +21,33 @@
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  asyncData() {
+    return { time: Date.now(), server: process.server };
+  },
+
+  data: () => ({ contents: [] as string[] }),
+
+  async mounted() {
+    const errorMessage =
+      'Your serverless functions most likely are not running. Use vercel dev instead of npm run dev.';
+
+    this.contents = await Promise.all([
+      // fetch('/api/hello').then((res) => res.text()),
+      // fetch('/api/contents/omgthisworks').then((res) => res.text()),
+
+      this.$axios
+        .$get('/api/hello')
+        .then((res) => res.text())
+        .catch((err) => console.log(`${errorMessage} ${err}.`)),
+
+      this.$axios
+        .$get('/api/contents/omgthisworks')
+        .then((res) => res.text())
+        .catch((err) => console.log(`${errorMessage} ${err}.`)),
+    ]);
+  },
+});
 </script>
 
 <style>
